76946ea909dc6b9fee9a957eebee3fee7937220b
Artur Mir <easy0@inbox.ru>
2026-07-13 13:33:06 +0500

    Stub and tests for add

--- src/main/java/org/example/phoneBook/PhoneBook.java
+++ src/main/java/org/example/phoneBook/PhoneBook.java
@@ -1,5 +1,22 @@
 package org.example.phoneBook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    private final Map<String, String> contacts = new HashMap<>();
+
+    public int add(String name, String phoneNumber) {
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Имя контакта не может быть пустым");
+        }
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            throw new IllegalArgumentException("Номер телефона не может быть пустым");
+        }
+
+        contacts.put(name, phoneNumber);
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,45 @@
+import org.example.phoneBook.PhoneBook;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PhoneBookTest {
 
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testAddFirstContact() {
+        assertEquals(1, phoneBook.add("Oleg", "+79191452949"));
+    }
+
+    @Test
+    public void testAddTwoContacts() {
+        assertEquals(1, phoneBook.add("Marina", "+781284814"));
+        assertEquals(2, phoneBook.add("Olga", "+781284812314"));
+    }
+
+    @Test
+    public void testAddTwoSameContacts() {
+        assertEquals(1, phoneBook.add("Marina", "+781284814"));
+        assertEquals(1, phoneBook.add("Marina", "+7812848143"));
+    }
+
+    @Test
+    public void testNameIsEmpty() {
+        assertEquals("Имя контакта не может быть пустым",
+                    assertThrows(IllegalArgumentException.class, () -> phoneBook.add("", "+124190284")).getMessage());
+    }
+
+    @Test
+    public void testNumberIsEmpty() {
+        assertEquals("Номер телефона не может быть пустым",
+                assertThrows(IllegalArgumentException.class, () -> phoneBook.add("Michail", "")).getMessage());
+    }
 }
